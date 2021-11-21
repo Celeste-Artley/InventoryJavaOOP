a89cec337b455f42ca1e2914de4bb3494b11d21d
Celeste Artley <90732656+Celeste-Artley@users.noreply.github.com>
2021-11-20 20:07:29 -0600

    updated the JavaDocs for ItemDatabase.java This database is not yet functonal but it has proper documentation.

--- src/invproject/Category.java
+++ src/invproject/Category.java
@@ -10,9 +10,14 @@ package invproject;
  */
 public class Category {
     private String name = "";
+    
     public Category(String name)
     {
         this.name = name;
+    }
+
+    Category(Category c) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     public String getName()
     {
--- src/invproject/ItemDatabase.java
+++ src/invproject/ItemDatabase.java
@@ -8,13 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * This is a Static variable database of all Item objects. 
  * @author Celeste Artley
  */
 public class ItemDatabase implements IDatabase<String, Item> {
-    private List<Item> items = new ArrayList<Item>();
+    private List<Item> items = new ArrayList<>();
+    private final String saveLocation = "items.txt";
     
-    void ItemDatabase()
+    public ItemDatabase()
     {
         items = Load();
     }
@@ -23,10 +24,23 @@ public class ItemDatabase implements IDatabase<String, Item> {
     {
         return items;
     }
+    
+    /**
+     *Takes a Item u as a argument and adds it to the Items database.
+     * @param i 
+     */
+    @Override
     public void Create(Item i)
     {
         items.add(i);
     }
+    
+    /**
+     * Reads from the database and looks for a Item by String s (name)
+     * @param s
+     * @return 
+     */
+    @Override
     public Item Read(String s) {
         Item val = null;
         for (Item i : items)
@@ -38,37 +52,82 @@ public class ItemDatabase implements IDatabase<String, Item> {
         }
         return val;
     }
-    public void UpdateCategory(String s, String c)
+    
+    /**
+     * Takes in a Item and a String to find and replace with the new 
+     * Item (value). Uses the String (username) to lookup the proper It to 
+     * replace.
+     * @param itemName
+     * @param value 
+     */
+    @Override
+    public void Update(String itemName, Item value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    /**
+     * Updates a Category on the item by taking in a Item Name (itemName)
+     * a Category Name(s), and a Category ( c) Finds the item to replace the 
+     * Category with (itemName) then find the Item's Category with (s) and 
+     * replaces their Category with (c)
+     * @param itemName
+     * @param s
+     * @param c 
+     */
+    public void UpdateCategory(String itemName, String s, Category c)
     {
         for (Item i : items)
         {
-            if(i.getName() == s)
+            if(i.getName() == null ? s == null : i.getName().equals(s))
             {
                Category newCat = new Category(c);
                i.setCategory(newCat);
             }
         }
     }
-    public void UpdateLongDesc(String s, String l)
+    
+    /**
+     * Updates a Long Description on the item by taking in a Item Name (itemName)
+     * a String Description(s), and a String (s) Finds the item to replace the 
+     * Long Description with (itemName) then replaces said string with String (s)
+     * @param itemName
+     * @param s
+     */
+    public void UpdateLongDesc(String itemName, String s)
     {
         for (Item i : items)
         {
-            if(i.getName() == s)
+            if(i.getName().equals(itemName))
             {
-               i.setlDescription(l);
+               i.setlDescription(s);
             }
         }
     }
-    public void UpdateShortDesc(String s, String d)
+    
+    /**
+     * Updates a Short Description on the item by taking in a Item Name (itemName)
+     * a String Description(s), and a String (s) Finds the item to replace the 
+     * Short Description with (itemName) then replaces said string with String (s)
+     * @param itemName
+     * @param s
+     */
+    public void UpdateShortDesc(String itemName, String s)
     {
         for (Item i : items)
         {
-            if(i.getName() == s)
+            if(i.getName().equals(itemName))
             {
-               i.setsDescription(d);
+               i.setsDescription(s);
             }
         }
     }
+    
+    /**
+     * Takes in a String to lookup from the database a Item then removes it
+     * from the database.
+     * @param s 
+     */
+    @Override
     public void Delete(String s)
     {
         for (Item i : items)
@@ -79,18 +138,22 @@ public class ItemDatabase implements IDatabase<String, Item> {
             }
         }
     }
+    
+    /**
+     * Saves the database to a text document in the saveLocation.
+     */
     public void Save()
     {
         
     }
+    
+    /**
+     * pulls the data from the saveLocation and puts it in the database
+     * @return 
+     */
     private List<Item> Load()
     {
         List<Item> items = new ArrayList<Item>();
         return items;
-    }
-
-    @Override
-    public void Update(String itemName, Item value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    }  
 }
